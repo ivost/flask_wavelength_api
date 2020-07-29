f6d3265efa57899bf35df5e290b7a605f4b5d0da
Mike Coleman <mikegcoleman@gmail.com>
2020-07-29 15:24:05 -0700

    added error handling

--- api.py
+++ api.py
@@ -56,54 +56,54 @@ def classify():
 
     print(response)
 
-    if response.code:
-        print (response.code)
+    if response['code']:
+        print (response['code'])
+    else:
+        # extract the coordinates and label from the returned data
+        results = response[0]
+        for result in results:
+            coordinates = str_to_tup(results[result])
+            label = result
 
-    # extract the coordinates and label from the returned data
-    results = response[0]
-    for result in results:
-        coordinates = str_to_tup(results[result])
-        label = result
+        position = (10,50)
 
-    position = (10,50)
+        cv2.putText(
+            image, #numpy array on which text is written
+            label, #text
+            position, #position at which writing has to start
+            cv2.FONT_HERSHEY_SIMPLEX, #font family
+            1, #font size
+            (209, 80, 0, 255), #font color
+            3) #font stroke
 
-    cv2.putText(
-        image, #numpy array on which text is written
-        label, #text
-        position, #position at which writing has to start
-        cv2.FONT_HERSHEY_SIMPLEX, #font family
-        1, #font size
-        (209, 80, 0, 255), #font color
-        3) #font stroke
+        start_point = (int(coordinates[0][0]), int(coordinates[0][1]))
+        end_point = (int(coordinates[1][0]), int(coordinates[1][1]))
 
-    start_point = (int(coordinates[0][0]), int(coordinates[0][1]))
-    end_point = (int(coordinates[1][0]), int(coordinates[1][1]))
+        # Blue color in BGR
+        color = (255, 0, 0)
 
-    # Blue color in BGR
-    color = (255, 0, 0)
+        # Line thickness of 2 px
+        thickness = 2
 
-    # Line thickness of 2 px
-    thickness = 2
+        # Using cv2.rectangle() method
+        # Draw a rectangle with blue line borders of thickness of 2 px
+        img = cv2.rectangle(image, start_point, end_point, color, thickness)
 
-    # Using cv2.rectangle() method
-    # Draw a rectangle with blue line borders of thickness of 2 px
-    img = cv2.rectangle(image, start_point, end_point, color, thickness)
+        s3 = boto3.resource('s3')
+        object = s3.Object('my_bucket_name', 'my/key/including/filename.txt')
+        object.put(Body=img)
 
-    s3 = boto3.resource('s3')
-    object = s3.Object('my_bucket_name', 'my/key/including/filename.txt')
-    object.put(Body=img)
+        img = Image.fromarray(img.astype("uint8"))
+        rawBytes = io.BytesIO()
+        img.save(rawBytes, "png")
+        rawBytes.seek(0)
+        img_base64 = str(base64.b64encode(rawBytes.read()))
 
-    img = Image.fromarray(img.astype("uint8"))
-    rawBytes = io.BytesIO()
-    img.save(rawBytes, "png")
-    rawBytes.seek(0)
-    img_base64 = str(base64.b64encode(rawBytes.read()))
+        response = jsonify({ "code": 200,
+            "message": "OK",
+            "image": img_base64
+            })
 
-    response = jsonify({ "Status": 200,
-        "Message": "Ok",
-        "Image": img_base64
-        })
-
-    return response
+        return response
 
 app.run(host="0.0.0.0")
